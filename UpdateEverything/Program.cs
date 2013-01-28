4d4f37c9594d13317e0d8ee76757e86cfae9cd0a
Mark Smith <tntc.tig@gmail.com>
2013-01-27 21:24:42 -0500

    Adding differentiation between types of updates.

--- UpdateEverything/Program.cs
+++ UpdateEverything/Program.cs
@@ -10,67 +10,68 @@ namespace UpdateEverything
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            UpdateSession session = new UpdateSession();
-            ISearchResult uResult;
+        //static void Main(string[] args)
+        //{
+        //    UpdateSession session = new UpdateSession();
+        //    ISearchResult uResult;
 
-            Boolean moreUpdates = true;
-            while (moreUpdates)
-            {
+        //    Boolean moreUpdates = true;
+        //    while (moreUpdates)
+        //    {
 
-                uResult = CheckForUpdates(session);
+        //        uResult = CheckForUpdates(session);
 
-                UpdateCollection toInstallAutomatically = getAutoUpdates(uResult);
+        //        UpdateCollection toInstallAutomatically = getAutoUpdates(uResult);
 
-                if (toInstallAutomatically.Count > 0)
-                {
-                    IInstallationResult installationRes = installUpdates(session, toInstallAutomatically);
+        //        if (toInstallAutomatically.Count > 0)
+        //        {
+        //            IInstallationResult installationRes = installUpdates(session, toInstallAutomatically);
 
-                    if (installationRes.RebootRequired)
-                    {
-                        Console.WriteLine("Rebooting to finish installation!");
-                        moreUpdates = false;
-                        System.Diagnostics.Process.Start("ShutDown", "/r");
-                    }
+        //            if (installationRes.RebootRequired)
+        //            {
+        //                Console.WriteLine("Rebooting to finish installation!");
+        //                moreUpdates = false;
+        //                System.Diagnostics.Process.Start("ShutDown", "/r");
+        //            }
 
-                }
-                else
-                {
-                    moreUpdates = false;
-                    System.Console.WriteLine("Updates Complete! Press a key to continue.");
-                    System.Console.ReadKey();
-                }
-            }
-        }
+        //        }
+        //        else
+        //        {
+        //            moreUpdates = false;
+        //            System.Console.WriteLine("Updates Complete! Press a key to continue.");
+        //            System.Console.ReadKey();
+        //        }
+        //    }
+        //}
 
-        private static ISearchResult CheckForUpdates(UpdateSession session)
-        {
-            ISearchResult uResult;
-            Console.WriteLine("Checking for updates!");
-            session = new UpdateSession();
-            IUpdateSearcher uSearcher = session.CreateUpdateSearcher();
-            uResult = uSearcher.Search("IsInstalled = 0 and Type='Software'");
-            return uResult;
-        }
+        //private static ISearchResult CheckForUpdates(UpdateSession session)
+        //{
+        //    ISearchResult uResult;
+        //    Console.WriteLine("Checking for updates!");
+        //    session = new UpdateSession();
+        //    IUpdateSearcher uSearcher = session.CreateUpdateSearcher();
+        //    uResult = uSearcher.Search("IsInstalled = 0 and Type='Software'");
+        //    return uResult;
+        //}
 
         private static IInstallationResult installUpdates(UpdateSession session, UpdateCollection toInstallAutomatically)
         {
             Console.WriteLine("Downloading {0} updates", toInstallAutomatically.Count);
             UpdateDownloader downloader = session.CreateUpdateDownloader();
-            
+
             downloader.Updates = toInstallAutomatically;
             downloader.Download();
             UpdateCollection updatesToInstall = new UpdateCollection();
             foreach (IUpdate update in toInstallAutomatically)
             {
+
                 if (update.IsDownloaded)
                 {
                     updatesToInstall.Add(update);
                 }
 
             }
-            Console.WriteLine("Installing {0} updates",updatesToInstall.Count);
+            Console.WriteLine("Installing {0} updates", updatesToInstall.Count);
             IUpdateInstaller installer = session.CreateUpdateInstaller();
             installer.Updates = updatesToInstall;
             IInstallationResult installtionRes = installer.Install();
@@ -84,33 +85,36 @@ namespace UpdateEverything
             UpdateCollection toInstallAutomatically = new UpdateCollection();
             foreach (IUpdate update in found.Updates)
             {
-
-                if (!update.EulaAccepted)
+                if (update.MsrcSeverity.Equals("Critical"))
                 {
-                    try
+
+
+                    if (!update.EulaAccepted)
                     {
-                        update.AcceptEula();
+                        try
+                        {
+                            update.AcceptEula();
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Unable to accept EULA on update " + update.Title);
+                        }
                     }
-                    catch (Exception)
+
+                    if (update.EulaAccepted && !update.InstallationBehavior.CanRequestUserInput)
                     {
-                        Console.WriteLine("Unable to accept EULA on update " + update.Title);
+                        toInstallAutomatically.Add(update);
+                    }
+
+                    // works for service packs, even though CanRequestUserInput is true. however, IE9 blocks progress.
+                    // This currently will try to install anything wtih "service pack" in the name. No bueno.
+                    if (update.EulaAccepted && update.Title.Contains("Service Pack"))
+                    {
+                        toInstallAutomatically.Add(update);
+                        Console.WriteLine("Service pack found!");
+                        Console.WriteLine(update.Title);
                     }
                 }
-
-                if (update.EulaAccepted && !update.InstallationBehavior.CanRequestUserInput)
-                {
-                    toInstallAutomatically.Add(update);
-                }
-
-                // works for service packs, even though CanRequestUserInput is true. however, IE9 blocks progress.
-                // This currently will try to install anything wtih "service pack" in the name. No bueno.
-                if (update.EulaAccepted && update.Title.Contains("Service Pack"))
-                {
-                    toInstallAutomatically.Add(update);
-                    Console.WriteLine("Service pack found!");
-                    Console.WriteLine(update.Title);
-                }
-
 
             }
             if (toInstallAutomatically.Count == 1)
---
+++ UpdateEverything/TestUpdates.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WUApiLib;
+using System.Web.Script.Serialization;
+
+
+namespace UpdateEverything
+{
+    class TestUpdates
+    {
+        static void Main(string[] args)
+        {
+            UpdateSession session = new UpdateSession();
+            ISearchResult uResult;
+
+            Boolean moreUpdates = true;
+            while (moreUpdates)
+            {
+
+                uResult = CheckForUpdates(session);
+
+                getAutoUpdates(uResult);
+            }
+        }
+
+        private static ISearchResult CheckForUpdates(UpdateSession session)
+        {
+            ISearchResult uResult;
+            Console.WriteLine("Checking for updates!");
+            session = new UpdateSession();
+            IUpdateSearcher uSearcher = session.CreateUpdateSearcher();
+            uResult = uSearcher.Search("IsInstalled = 0 and Type='Software'");
+            return uResult;
+        }
+
+//        class Foo {
+//    public int A {get;set;}
+//    public string B {get;set;}
+//}
+//...
+//Foo foo = new Foo {A = 1, B = "abc"};
+//foreach(var prop in foo.GetType().GetProperties()) {
+//    Console.WriteLine("{0}={1}", prop.Name, prop.GetValue(foo, null));
+//}
+
+
+        static void getAutoUpdates(ISearchResult found)
+        {
+            Console.WriteLine("Accepting EULAs");
+            UpdateCollection toInstallAutomatically = new UpdateCollection();
+            foreach (IUpdate update in found.Updates)
+            {
+                Console.WriteLine(update.Title);
+                /*
+                 * Critical, Important, Moderate and Low. This field may also be blank.
+                 * Blank does not mean "optional update", it just means "not a security-critical update. Probably."
+                 */
+               Console.WriteLine("MsrcSeverity:         " + update.MsrcSeverity);
+               //  IUpdate::AutoSelectOnWebSites property (Windows)
+               //  shows whether or not an update is selected by default?
+                Console.WriteLine("AutoSelectOnWebSites: " + update.AutoSelectOnWebSites);
+                Console.WriteLine("DownloadPriority:     " + update.DownloadPriority);
+                Console.WriteLine("IsMandatory:          " + update.IsMandatory);
+                Console.WriteLine("Eula Accepted:        " + update.EulaAccepted);
+
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
